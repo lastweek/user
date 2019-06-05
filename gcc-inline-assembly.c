5397a70f5cff6c7c77f5d5ed64e662c8495e81ac
Yizhou Shan <ys@purdue.edu>
2019-06-05 18:32:59 -0400

    Update

---
+++ gcc-inline-assembly.c
@@ -0,0 +1,12 @@
+#include "compiler.h"
+
+static __always_inline unsigned long
+current_stack_pointer(void)
+{
+	unsigned long sp;
+	asm volatile (
+		"movq %%rsp, %0\n"
+		: "=r" (sp)
+	);
+	return sp;
+}
